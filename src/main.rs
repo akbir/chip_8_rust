6c98243f563881b8d23f4a7ed017146c2d0f8b49
am-khan <akbir.94@gmail.com>
2020-08-23 22:58:02 +0100

    added some test roms and cases

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use processor::Processor;
 use sdl;
+use sdl::event::Event;
+
 
 mod processor;
 mod display;
@@ -8,11 +10,23 @@ mod keyboard;
 fn main() {
     let mut my_chip8 = Processor::new();
     my_chip8.reset();
+    my_chip8.load_rom("roms/pong");
 
     sdl::init(&[sdl::InitFlag::Video, sdl::InitFlag::Audio, sdl::InitFlag::Timer]);
-    my_chip8.execute_cycle();
-
+    'main : loop {
+        'event : loop {
+            match sdl::event::poll_event() {
+                Event::Quit                  => break 'main,
+                Event::None                  => break 'event,
+                Event::Key(key, state, _, _) => my_chip8.keyboard.press(key, state),
+                _                            => {}
+            }
+        }
+        my_chip8.execute_cycle();
+        my_chip8.display.draw_screen();
+    }
 
     sdl::quit();
+
 }
 
--- src/processor.rs
+++ src/processor.rs
@@ -1,4 +1,3 @@
-use rand::Rng;
 use crate::display::Display;
 use crate::keyboard::Keyboard;
 
@@ -28,6 +27,9 @@ fn read_word(memory: [u8; 4096], index: u16) -> u16 {
     (memory[index as usize] as u16) << 8
         | (memory[(index + 1) as usize] as u16)
 }
+
+fn not_implemented(op: u16, pc: u16) { panic!("Not implemented:: op: {:x}, pc: {:x}", op, pc) }
+
 
 impl Processor {
     pub fn new() -> Processor {
@@ -61,12 +63,23 @@ impl Processor {
         self.memory[ 0 .. 80].copy_from_slice(&FONT_SET);
     }
 
+    pub fn load_rom(&mut self, rom: &str){
+        match std::fs::read(rom) {
+            Ok(bytes) => {
+                self.memory[ 0x200 .. 0x200 + bytes.len() as usize].copy_from_slice(&bytes);
+            },
+            Err(e) => panic!("{}", e)
+        }
+
+    }
+
+
     pub fn execute_cycle(&mut self) {
         // fetch opcode
-        let _opcode = read_word(self.memory, self.index_register);
+        let opcode = read_word(self.memory, self.program_counter);
 
         // execute opcode
-        self.execute_opcode(_opcode);
+        self.execute_opcode(opcode);
 
         // update timers
         self.decrement_delay_timer();
@@ -83,13 +96,15 @@ impl Processor {
         // helper addresses
         let nnn = opcode & 0x0FFF;
         let nn = (opcode & 0x00FF) as u8;
-        let n = opcode & 0x000F;
+        let n = (opcode & 0x000F) as u8;
 
         // check registers
         let x = ((opcode & 0x0F00) >> 8) as usize;
         let y = ((opcode & 0x00F0) >> 4) as usize;
         let vx = self.register[x];
         let vy = self.register[y];
+
+        println!("Nibbles: {} {} {} {}", op_1, op_2, op_3, op_4);
 
         // we read the opcode so move program counter forward
         self.program_counter += 2;
@@ -127,7 +142,9 @@ impl Processor {
             (0x6, _, _, _) => self.register[x] = nn,
 
             // Adds NN to VX
-            (0x7, _, _, _) => self.register[x] += nn,
+            (0x7, _, _, _) => {
+                self.register[x] += nn
+            },
 
             // Sets VX to the value of VY.
             (0x8, _, _, 0x0) => self.register[x] = vy,
@@ -151,7 +168,7 @@ impl Processor {
             // Subtract VY from VX. VF is set to 0 if there's a borrow
             (0x8, _, _, 0x5) => {
                 let (res, overflow) = vx.overflowing_sub(vy);
-                self.register[0xF] = !overflow as u8;
+                self.register[0xF] = (!overflow) as u8;
                 self.register[x] = res;
             }
 
@@ -184,12 +201,12 @@ impl Processor {
             (0xB, _, _, _) => self.index_register = nnn + self.register[0] as u16,
 
             // Set VX to random number and NN
-            (0xC, _, _, _) => self.register[x] = nn & rand::thread_rng().gen_range(1, 255),
+            (0xC, _, _, _) => self.register[x] = nn & rand::random::<u8>(),
 
             // Draws a sprite at coordinate (VX, VY), set VF to 1 if pixels unset else 0
             (0xD, _, _, _) => {
                 let sprite = &self.memory
-                    [self.index_register as usize .. (self.index_register + n as u16) as usize];
+                    [self.index_register as usize..(self.index_register + n as u16) as usize];
                 self.register[0xF] = self.display.draw(vx as usize, vy as usize, sprite)
             },
 
@@ -207,8 +224,8 @@ impl Processor {
                 // move back register (no key is pressed)
                 self.program_counter -= 2;
 
-                for key in 0 .. 0xF{
-                    if self.keyboard.pressed(key){
+                for key in 0..0xF {
+                    if self.keyboard.pressed(key) {
                         self.register[x] = key as u8;
                         self.program_counter += 2;
                     }
@@ -236,19 +253,18 @@ impl Processor {
 
             // Stores V0 to VX (including VX) in memory starting at address I
             (0xF, _, 0x5, 0x5) => self.memory
-                [self.index_register as usize .. (x + 1 + self.index_register as usize)]
+                [self.index_register as usize..(x + 1 + self.index_register as usize)]
                 .copy_from_slice(&self.register[0..x + 1]),
 
             // Fills V0 to VX (including VX) with values from memory starting at address I
             (0xF, _, 0x6, 0x5) => self.register[0..x + 1]
                 .copy_from_slice(&self.memory
-                    [self.index_register as usize .. (x + 1 + self.index_register as usize)]),
+                    [self.index_register as usize..(x + 1 + self.index_register as usize)]),
 
             // ...
-            (_, _, _, _) => ()
+            (_, _, _, _) => not_implemented(opcode, self.program_counter)
         }
-}
-
+    }
 
     fn decrement_delay_timer(&mut self) {
             if self.delay_timer > 0 {
@@ -269,3 +285,244 @@ static FONT_SET: [u8; 80] = [0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x2
 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0,
 0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0,
 0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80];
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn opcode_jp() {
+        let mut cpu = Processor::new();
+        cpu.execute_opcode(0x1A2A);
+        assert_eq!(cpu.program_counter, 0x0A2A, "the program counter is updated");
+    }
+
+    #[test]
+    fn opcode_call() {
+        let mut cpu = Processor::new();
+        let addr = 0x23;
+        cpu.program_counter = addr;
+
+        cpu.execute_opcode(0x2ABC);
+
+        assert_eq!(cpu.program_counter, 0x0ABC, "the program counter is updated to the new address");
+        assert_eq!(cpu.stack_pointer, 1, "the stack pointer is incremented");
+        assert_eq!(cpu.stack[0], addr + 2, "the stack stores the previous address");
+    }
+
+    #[test]
+    fn opcode_se_vx_byte() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 0xFE;
+
+        // vx == kk
+        cpu.execute_opcode(0x31FE);
+        assert_eq!(cpu.program_counter, 4, "the stack pointer skips");
+
+        // vx != kk
+        cpu.execute_opcode(0x31FA);
+        assert_eq!(cpu.program_counter, 6, "the stack pointer is incremented");
+    }
+
+    #[test]
+    fn opcode_sne_vx_byte() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 0xFE;
+
+        // vx == kk
+        cpu.execute_opcode(0x41FE);
+        assert_eq!(cpu.program_counter, 2, "the stack pointer is incremented");
+
+        // vx != kk
+        cpu.execute_opcode(0x41FA);
+        assert_eq!(cpu.program_counter, 6, "the stack pointer skips");
+    }
+
+    #[test]
+    fn opcode_se_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 1;
+        cpu.register[2] = 3;
+        cpu.register[3] = 3;
+
+        // vx == vy
+        cpu.execute_opcode(0x5230);
+        assert_eq!(cpu.program_counter, 4, "the stack pointer skips");
+
+        // vx != vy
+        cpu.execute_opcode(0x5130);
+        assert_eq!(cpu.program_counter, 6, "the stack pointer is incremented");
+    }
+
+    #[test]
+    fn opcode_sne_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 1;
+        cpu.register[2] = 3;
+        cpu.register[3] = 3;
+
+        // vx == vy
+        cpu.execute_opcode(0x9230);
+        assert_eq!(cpu.program_counter, 2, "the stack pointer is incremented");
+
+        // vx != vy
+        cpu.execute_opcode(0x9130);
+        assert_eq!(cpu.program_counter, 6, "the stack pointer skips");
+    }
+
+    #[test]
+    fn opcode_add_vx_kkk() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 3;
+
+        cpu.execute_opcode(0x7101);
+        assert_eq!(cpu.register[1], 4, "Vx was incremented by one");
+    }
+
+    #[test]
+    fn opcode_ld_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 3;
+        cpu.register[0] = 0;
+
+        cpu.execute_opcode(0x8010);
+        assert_eq!(cpu.register[0], 3, "Vx was loaded with vy");
+    }
+
+    #[test]
+    fn opcode_or_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[2] = 0b01101100;
+        cpu.register[3] = 0b11001110;
+
+        cpu.execute_opcode(0x8231);
+        assert_eq!(cpu.register[2], 0b11101110, "Vx was loaded with vx OR vy");
+    }
+
+    #[test]
+    fn opcode_and_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[2] = 0b01101100;
+        cpu.register[3] = 0b11001110;
+
+        cpu.execute_opcode(0x8232);
+        assert_eq!(cpu.register[2], 0b01001100, "Vx was loaded with vx AND vy");
+    }
+
+    #[test]
+    fn opcode_xor_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[2] = 0b01101100;
+        cpu.register[3] = 0b11001110;
+
+        cpu.execute_opcode(0x8233);
+        assert_eq!(cpu.register[2], 0b10100010, "Vx was loaded with vx XOR vy");
+    }
+
+    #[test]
+    fn opcode_add_vx_vy() {
+        let mut cpu = Processor::new();
+        cpu.register[1] = 10;
+        cpu.register[2] = 100;
+        cpu.register[3] = 250;
+
+        cpu.execute_opcode(0x8124);
+        assert_eq!(cpu.register[1], 110, "Vx was loaded with vx + vy");
+        assert_eq!(cpu.register[0xF], 0, "no overflow occurred");
+
+        cpu.execute_opcode(0x8134);
+        assert_eq!(cpu.register[1], 0x68, "Vx was loaded with vx + vy");
+        assert_eq!(cpu.register[0xF], 1, "overflow occurred");
+    }
+
+    #[test]
+    fn opcode_ld_i_vx() {
+        let mut cpu = Processor::new();
+        cpu.register[0] = 5;
+        cpu.register[1] = 4;
+        cpu.register[2] = 3;
+        cpu.register[3] = 2;
+        cpu.index_register = 0x300;
+
+        // load v0 - v2 into memory at i
+        cpu.execute_opcode(0xF255);
+        assert_eq!(cpu.memory[cpu.index_register as usize], 5, "V0 was loaded into memory at i");
+        assert_eq!(cpu.memory[cpu.index_register as usize + 1], 4, "V1 was loaded into memory at i + 1");
+        assert_eq!(cpu.memory[cpu.index_register as usize + 2], 3, "V2 was loaded into memory at i + 2");
+        assert_eq!(cpu.memory[cpu.index_register as usize + 3], 0, "i + 3 was not loaded");
+    }
+
+    #[test]
+    fn opcode_ld_b_vx() {
+        let mut cpu = Processor::new();
+        cpu.index_register = 0x300;
+        cpu.register[2] = 234;
+
+        // load v0 - v2 from memory at i
+        cpu.execute_opcode(0xF233);
+        assert_eq!(cpu.memory[cpu.index_register as usize], 2, "hundreds");
+        assert_eq!(cpu.memory[cpu.index_register as usize + 1], 3, "tens");
+        assert_eq!(cpu.memory[cpu.index_register as usize + 2], 4, "digits");
+    }
+
+    #[test]
+    fn opcode_ld_vx_i() {
+        let mut cpu = Processor::new();
+        cpu.index_register = 0x300;
+        cpu.memory[cpu.index_register as usize] = 5;
+        cpu.memory[cpu.index_register as usize + 1] = 4;
+        cpu.memory[cpu.index_register as usize + 2] = 3;
+        cpu.memory[cpu.index_register as usize + 3] = 2;
+
+
+        // load v0 - v2 from memory at i
+        cpu.execute_opcode(0xF265);
+        assert_eq!(cpu.register[0], 5, "V0 was loaded from memory at i");
+        assert_eq!(cpu.register[1], 4, "V1 was loaded from memory at i + 1");
+        assert_eq!(cpu.register[2], 3, "V2 was loaded from memory at i + 2");
+        assert_eq!(cpu.register[3], 0, "i + 3 was not loaded");
+    }
+
+    #[test]
+    fn opcode_ret() {
+        let mut cpu = Processor::new();
+        let addr = 0x23;
+        cpu.program_counter = addr;
+
+        // jump to 0x0ABC
+        cpu.execute_opcode(0x2ABC);
+        // return
+        cpu.execute_opcode(0x00EE);
+
+        assert_eq!(cpu.program_counter, 0x25, "the program counter is updated to the new address");
+        assert_eq!(cpu.stack_pointer, 0, "the stack pointer is decremented");
+    }
+
+
+    #[test]
+    fn opcode_ld_i_addr() {
+        let mut cpu = Processor::new();
+
+        cpu.execute_opcode(0x61AA);
+        assert_eq!(cpu.register[1], 0xAA, "V1 is set");
+        assert_eq!(cpu.program_counter, 2, "the program counter is advanced two bytes");
+
+        cpu.execute_opcode(0x621A);
+        assert_eq!(cpu.register[2], 0x1A, "V2 is set");
+        assert_eq!(cpu.program_counter, 4, "the program counter is advanced two bytes");
+
+        cpu.execute_opcode(0x6A15);
+        assert_eq!(cpu.register[10], 0x15, "V10 is set");
+        assert_eq!(cpu.program_counter, 6, "the program counter is advanced two bytes");
+    }
+
+    #[test]
+    fn opcode_axxx() {
+        let mut cpu = Processor::new();
+        cpu.execute_opcode(0xAFAF);
+
+        assert_eq!(cpu.index_register, 0x0FAF, "the 'i' register is updated");
+        assert_eq!(cpu.program_counter, 2, "the program counter is advanced two bytes");
+    }
+}
